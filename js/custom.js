66d5755d4a6a4641b52eecf687ac7a7cbd728d4b
mgdlnjp <84024317+mgdlnjp@users.noreply.github.com>
2022-10-13 16:30:01 +0200

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,170 @@
+(function ($) {
+
+  "use strict";
+
+    // COLOR MODE
+    $('.color-mode').click(function(){
+        $('.color-mode-icon').toggleClass('active')
+        $('body').toggleClass('dark-mode')
+    })
+
+    // HEADER
+    $(".navbar").headroom();
+
+    // PROJECT CAROUSEL
+    $('.owl-carousel').owlCarousel({
+    	items: 1,
+	    loop:true,
+	    margin:10,
+	    nav:true
+	});
+
+    // SMOOTHSCROLL
+    $(function() {
+      $('.nav-link, .custom-btn-link').on('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top - 49
+        }, 1000);
+        event.preventDefault();
+      });
+    });  
+
+    // TOOLTIP
+    $('.social-links a').tooltip();
+
+})(jQuery);
+
+/* 
+
+ThreeJs custom waves
+Original script by ThreeJS : https://threejs.org/examples/canvas_particles_waves.html
+Modified version for Cloudoru by Kevin Rajaram : http://kevinrajaram.com
+Date: 08/14/2014
+
+
+*/
+
+var SEPARATION = 40, AMOUNTX = 250, AMOUNTY = 50;
+
+var container;
+var camera, scene, renderer;
+/*
+
+if (window.WebGLRenderingContext){
+	renderer = new THREE.WebGLRenderer({ alpha: true, antialias: true });
+	}
+else {
+	renderer = new THREE.CanvasRenderer();
+	}
+*/
+
+var particles, particle, count = 0;
+
+var windowHalfX = window.innerWidth / 2;
+var windowHalfY = window.innerHeight / 2;
+
+init();
+animate();
+
+function init() {
+
+	container = document.createElement( 'div' );
+	document.body.appendChild( container );
+	if(container) {
+    	container.className += container.className ? ' waves' : 'waves';
+	}
+
+	camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 1, 10000 );
+	camera.position.y = 400; //changes how far back you can see i.e the particles towards horizon
+	camera.position.z = 500; //This is how close or far the particles are seen
+	
+	camera.rotation.x = 0.05;
+	
+	scene = new THREE.Scene();
+
+	particles = new Array();
+
+	var PI2 = Math.PI * 2;
+	var material = new THREE.SpriteCanvasMaterial( {
+
+		color: 0x939393, //changes color of particles
+		program: function ( context ) {
+
+			context.beginPath();
+			context.arc( 0, 0, 0.1, 0, PI2, true );
+			context.fill();
+
+		}
+
+	} );
+
+	var i = 0;
+
+	for ( var ix = 0; ix < AMOUNTX; ix ++ ) {
+
+		for ( var iy = 0; iy < AMOUNTY; iy ++ ) {
+
+			particle = particles[ i ++ ] = new THREE.Sprite( material );
+			particle.position.x = ix * SEPARATION - ( ( AMOUNTX * SEPARATION ) / 2 );
+			particle.position.z = iy * SEPARATION - ( ( AMOUNTY * SEPARATION ) - 10 );
+			scene.add( particle );
+
+		}
+
+	}
+
+	renderer = new THREE.CanvasRenderer();
+	renderer.setSize( window.innerWidth, window.innerHeight );
+	renderer.setClearColor( 0x00000, 1);
+	container.appendChild( renderer.domElement );
+
+	window.addEventListener( 'resize', onWindowResize, false );
+
+}
+
+function onWindowResize() {
+
+	windowHalfX = window.innerWidth / 2;
+	windowHalfY = window.innerHeight / 2;
+
+	camera.aspect = window.innerWidth / window.innerHeight;
+	camera.updateProjectionMatrix();
+
+	renderer.setSize( window.innerWidth, window.innerHeight );
+
+}
+
+function animate() {
+
+	requestAnimationFrame( animate );
+
+	render();
+
+}
+
+function render() {
+
+	var i = 0;
+
+	for ( var ix = 0; ix < AMOUNTX; ix ++ ) {
+
+		for ( var iy = 0; iy < AMOUNTY; iy ++ ) {
+
+			particle = particles[ i++ ];
+			particle.position.y = ( Math.sin( ( ix + count ) * 0.5 ) * 20 ) + ( Math.sin( ( iy + count ) * 0.5 ) * 20 );
+			particle.scale.x = particle.scale.y = ( Math.sin( ( ix + count ) * 0.3 ) + 2 ) * 4 + ( Math.sin( ( iy + count ) * 0.5 ) + 1 ) * 4;
+
+		}
+
+	}
+
+	renderer.render( scene, camera );
+
+	// This increases or decreases speed
+	count += 0.25;
+
+}
+
+
+
